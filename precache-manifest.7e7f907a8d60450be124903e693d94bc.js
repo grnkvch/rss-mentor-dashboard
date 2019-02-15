2267e737e2f16d2b1b5bab61c876d0f1385bd60d
grnkvch <grinkevich.vv@gmail.com>
2019-02-15 17:26:30 +0300

    release

---
+++ precache-manifest.7e7f907a8d60450be124903e693d94bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5280a2c0b2d5e5880bdc",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
+  },
+  {
+    "revision": "ae581c13604c0648620f",
+    "url": "/rss-mentor-dashboard/static/js/main.ae581c13.chunk.js"
+  },
+  {
+    "revision": "5af655b33b66d5368ffa",
+    "url": "/rss-mentor-dashboard/static/js/2.5af655b3.chunk.js"
+  },
+  {
+    "revision": "ae581c13604c0648620f",
+    "url": "/rss-mentor-dashboard/static/css/main.d4cecc1a.chunk.css"
+  },
+  {
+    "revision": "dbfd0cff09deb10d4292fb5ccf085edf",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
